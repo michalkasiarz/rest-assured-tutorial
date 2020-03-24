65157a54cabdcc2bece7e68c3b6b064bd2ac1ccd
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-24 09:58:59 +0100

    first test file added with basic imports

---
+++ src/test/java/GetRequestDemo.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+
+public class GetRequestDemo {
+    @Test
+    public void statusCodeVerification() {
+
+    }
+}
