a68c39f1c95791025e678a0842c572dae004aa67
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-24 11:07:27 +0100

    dependencies added, first complete test for response status

--- src/test/java/GetRequestDemo.java
+++ src/test/java/GetRequestDemo.java
@@ -1,10 +1,26 @@
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
 
 public class GetRequestDemo {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://maps.googleapis.com";
+        RestAssured.basePath = "/maps/api/";
+    }
+
     @Test
     public void statusCodeVerification() {
-
+        given()
+                .param("units", "imperial")
+                .param("origins", "Washington,DC")
+                .param("destinations", "New+York+City,NY")
+                .param("key", "")
+        .when()
+                .get("/distancematrix/json")
+        .then()
+                .statusCode(200);
     }
 }
